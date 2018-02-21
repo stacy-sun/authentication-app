eac9433287ac406bf45fcc2a08f0adf062e617d3
Stacy Sun <stacysun1206@gmail.com>
2018-02-20 18:22:57 -0800

    changes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,8 @@
-export const SIGN_UP = '/authentication-app/signup';
-export const SIGN_IN = '/authentication-app/signin';
-export const LANDING = '/authentication-app/';
-export const HOME = '/authentication-app/home';
-export const ACCOUNT = '/authentication-app/account';
-export const PASSWORD_FORGET = '/authentication-app/pw-forget';
-export const TEACHER = '/authentication-app/teacher';
-export const STUDENT = '/authentication-app/student';
+export const SIGN_UP = '#signup';
+export const SIGN_IN = '#signin';
+export const LANDING = '#';
+export const HOME = '#home';
+export const ACCOUNT = '#account';
+export const PASSWORD_FORGET = '#pw-forget';
+export const TEACHER = '#teacher';
+export const STUDENT = '#student';
