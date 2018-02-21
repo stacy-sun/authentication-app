300afbcb818d960952fa0295a4cea4193e1762e5
Stacy Sun <stacysun1206@gmail.com>
2018-02-20 19:20:47 -0800

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ const App = () =>
     <div>
       <Navigation />
 
-      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+      <Route path={routes.LANDING} component={() => <LandingPage />} />
       <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
       <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
       <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
