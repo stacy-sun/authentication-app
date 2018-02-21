4bcefe3ae773ff951b14475016df293e61f4203f
Stacy Sun <stacysun1206@gmail.com>
2018-02-20 19:25:51 -0800

    changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,21 @@
 import * as firebase from 'firebase';
 
 const prodConfig = {
-  apiKey: "AIzaSyBRA6IiicWrMXgNy6RTnP72D4jLe8E2Qm0",
-  authDomain: "auth-app-production.firebaseapp.com",
-  databaseURL: "https://auth-app-production.firebaseio.com",
-  projectId: "auth-app-production",
-  storageBucket: "auth-app-production.appspot.com",
-  messagingSenderId: "703155886519"
+  // apiKey: "AIzaSyBRA6IiicWrMXgNy6RTnP72D4jLe8E2Qm0",
+  // authDomain: "auth-app-production.firebaseapp.com",
+  // databaseURL: "https://auth-app-production.firebaseio.com",
+  // projectId: "auth-app-production",
+  // storageBucket: "auth-app-production.appspot.com",
+  // messagingSenderId: "703155886519"
 };
 
 const devConfig = {
-  apiKey: "AIzaSyCFcRex8gLd1Jwde20HP2ue6iV43w7ZsnI",
-  authDomain: "authentication-app-45b53.firebaseapp.com",
-  databaseURL: "https://authentication-app-45b53.firebaseio.com",
-  projectId: "authentication-app-45b53",
-  storageBucket: "authentication-app-45b53.appspot.com",
-  messagingSenderId: "304269722396"
+  // apiKey: "AIzaSyCFcRex8gLd1Jwde20HP2ue6iV43w7ZsnI",
+  // authDomain: "authentication-app-45b53.firebaseapp.com",
+  // databaseURL: "https://authentication-app-45b53.firebaseio.com",
+  // projectId: "authentication-app-45b53",
+  // storageBucket: "authentication-app-45b53.appspot.com",
+  // messagingSenderId: "304269722396"
 };
 
   const config = process.env.NODE_ENV === 'production'
