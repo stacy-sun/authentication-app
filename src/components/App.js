4c021f7d427ecd0d0e8389d31ccff622bbfc6109
Stacy Sun <stacysun1206@gmail.com>
2018-02-20 19:35:59 -0800

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -28,11 +28,11 @@ const App = () =>
       <Navigation />
 
       <Route exact path={routes.LANDING} component={() => <LandingPage />} />
-      <Route path={routes.SIGN_UP} component={() => <SignUpPage />} />
-      <Route path={routes.SIGN_IN} component={() => <SignInPage />} />
-      <Route path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
-      <Route path={routes.HOME} component={() => <HomePage />} />
-      <Route path={routes.ACCOUNT} component={() => <AccountPage />} />
+      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+      <Route exact path={routes.HOME} component={() => <HomePage />} />
+      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
     </div>
     </MuiThemeProvider>
     </BrowserRouter>
